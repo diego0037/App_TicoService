8b6d4d6faf6ccd333821f01e73ed78f41fb9fd6e
Diego <diegomendez-0037@hotmail.com>
2017-04-19 22:56:54 -0600

    PaginaServicioListo

--- resources/views/PaginasWeb/service.blade.php
+++ resources/views/PaginasWeb/service.blade.php
@@ -7,21 +7,20 @@
 @section('content')
 <div class="container">
 
+<div class="row">
+  <div class="col-md-offset-2 col-md-8 col-md-offset-2" id="panelServicio">
+    <table class="table table-striped" id="tablaService">
 
-<div class="col-md-2"></div>
-<div class="col-md-8">
-  <table class="table table-striped">
-
-   <tbody>
-     <?php
-           echo "<tr><td>SERVICIO</td><td>$service->name</td></tr>
-                  <tr><td>DESCRIPCIÓN</td><td>$service->description</td>
-                  <td><a href='collaboratorService/$service->id' role='button'
-                                    class='btn btn-success'>Agregar</a></td></tr>";
-      ?>
-   </tbody>
- </table>
-
+     <tbody>
+       <?php
+             echo "<tr><td>SERVICIO</td><td>$service->name</td></tr>
+                    <tr><td>DESCRIPCIÓN</td><td>$service->description</td>
+                    <td><a href='collaboratorService/$service->id' role='button'
+                                      class='btn btn-success'>Agregar</a></td></tr>";
+        ?>
+     </tbody>
+    </table>
+  </div>
 </div>
 
 </div>
